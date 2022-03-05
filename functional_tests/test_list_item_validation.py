ab47eb1bb68e37f045394f937544ba9a5f2e9215
snymanc <claytonsnyman@gmail.com>
2022-03-05 09:32:21 +0200

    Using the Existing List Item Form in the List View

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -57,6 +57,6 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.browser.find_element_by_css_selector('.has-error').text.strip(),
             "You've already got this in your list"
         ))
