b94abad7cb83d85bac60a56f8a63849517fcec09
snymanc <claytonsnyman@gmail.com>
2022-03-15 06:40:06 +0200

    Adding messages to login

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,18 @@
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        'Use this link to log in',
         'noreply@superlists',
         [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to login."
     )
     return redirect('/')
