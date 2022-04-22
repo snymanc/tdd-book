6d5911f27c83469ee512226dec6b60fb1522e005
snymanc <claytonsnyman@gmail.com>
2022-04-22 07:36:40 +0200

    Page pattern

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,6 +1,7 @@
 import os
 from selenium import webdriver
 from .base import FunctionalTest
+from .list_page import ListPage
 
 
 def quit_if_possible(browser):
@@ -30,13 +31,15 @@ class SharingTest(FunctionalTest):
         # Edith goes to the home page and start a list
         self.browser = edith_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('Get help')
+        list_page = ListPage(self).add_list_item('Get help')
 
         # She notices a "Share this list" option
-        share_box = self.browser.find_element_by_css_selector(
-            'input[name="share"]'
-        )
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        # She shares her list.
+        # The page updates to say that it's share with Oniciferous
+        list_page.share_list_with('oniciferous@example.com')
