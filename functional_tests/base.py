da722fe4b93ab1a7ef8a2514e656496fb8d0ea45
snymanc <claytonsnyman@gmail.com>
2022-04-22 07:01:32 +0200

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,11 +1,13 @@
 import os
 from datetime import datetime
 import time
+from django.conf import settings
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-from .server_tools import reset_database
+from .management.commands.create_session import create_pre_authenticated_session
+from .server_tools import create_session_on_server, reset_database
 
 MAX_WAIT = 20
 SCREEN_DUMP_LOCATION = os.path.join(
@@ -45,7 +47,7 @@ class FunctionalTest(StaticLiveServerTestCase):
             if not os.path.exists(SCREEN_DUMP_LOCATION):
                 os.makedirs(SCREEN_DUMP_LOCATION)
             for ix, handle in enumerate(self.browser.window_handles):
-                self.windowid = ix
+                self._windowid = ix
                 self.browser.switch_to_window(handle)
                 self.take_screenshot()
                 self.dump_html()
@@ -109,3 +111,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        # to set a cookie we need to first visit the domain
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/'
+        ))
